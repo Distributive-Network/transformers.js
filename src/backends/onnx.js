57daf5f8d447c14f83fff181b787c3316711690b
Tom Tang <xmader@distributive.network>
2025-04-01 23:46:51 +0000

    set `webgpu` and `wasm` as supportedDevices

--- src/backends/onnx.js
+++ src/backends/onnx.js
@@ -59,8 +59,9 @@ const ORT_SYMBOL = Symbol.for('onnxruntime');
 if (ORT_SYMBOL in globalThis) {
     // If the JS runtime exposes their own ONNX runtime, use it
     ONNX = globalThis[ORT_SYMBOL];
-    supportedDevices.push('cpu');
-    defaultDevices = ['cpu'];
+    supportedDevices.push('webgpu');
+    supportedDevices.push('wasm');
+    defaultDevices = ['wasm'];
 
 } else if (apis.IS_NODE_ENV) {
     ONNX = ONNX_NODE.default ?? ONNX_NODE;
